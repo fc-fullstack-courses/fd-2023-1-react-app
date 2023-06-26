6a3c3bfe4f6c076c01c0eff07d712056bf998890
Kyrylo <fresh.js.ftd.@gmail.com>
2023-06-26 15:50:42 +0300

    Add class Component example

--- src/index.js
+++ src/index.js
@@ -4,14 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const elem1 = React.createElement(
-  'h1',
-  { id: 'mainHeading', className: 'class1' },
-  'Hello react!'
-);
+// классовый компонент
+class Heading extends React.Component {
+  render() {
+    const h1 = React.createElement(
+      'h1',
+      { id: 'mainHeading', className: 'class1' },
+      'My Site'
+    );
+
+    return React.createElement('header', {}, h1);
+  }
+}
+
+const header = React.createElement(Heading);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(elem1);
+root.render(header);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
