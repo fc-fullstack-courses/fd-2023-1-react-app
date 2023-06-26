62f86adc3470d8b3e769857bf734c862c2c37871
Kyrylo <fresh.js.ftd.@gmail.com>
2023-06-26 16:10:56 +0300

    Add props examples

--- src/index.js
+++ src/index.js
@@ -7,29 +7,44 @@ import reportWebVitals from './reportWebVitals';
 // классовый компонент
 // class Heading extends React.Component {
 //   render() {
+//     const { headerText, headerTitle } = this.props;
+//     // console.log(this.props);
+
+//     const h1Text = headerText ? headerText : 'My Site';
+
 //     const h1 = React.createElement(
 //       'h1',
 //       { id: 'mainHeading', className: 'class1' },
-//       'My Site'
+//       h1Text
 //     );
 
-//     return React.createElement('header', {}, h1);
+//     return React.createElement('header', { title: headerTitle }, h1);
 //   }
 // }
 
 // функциональный компонент
-function Heading() {
-  
+function Heading(props) {
+  const { headerText, headerTitle } = props;
+
+  const h1Text = headerText ? headerText : 'My Site';
+
   const h1 = React.createElement(
     'h1',
     { id: 'mainHeading', className: 'class1' },
-    'My Site'
+    h1Text
   );
 
-  return React.createElement('header', {}, h1);
+  return React.createElement('header', { title: headerTitle }, h1);
 }
 
-const header = React.createElement(Heading);
+// const header = React.createElement(Heading, {
+//   headerText: 'Prop Site',
+// });
+
+const header = React.createElement(Heading, {
+  headerText: 'Rozetka',
+  headerTitle: 'text for title',
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(header);
