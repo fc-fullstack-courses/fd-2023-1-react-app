37610fbf1c9d7c24a465de5bd3c35f597e2ea8a3
Kyrylo <fresh.js.ftd.@gmail.com>
2023-06-28 15:21:39 +0300

    Add greeting list example

--- src/App.js
+++ src/App.js
@@ -1,36 +1,94 @@
+import React from 'react';
 import './App.css';
 import Heading from './components/Heading';
 import Counter from './components/Counter';
 import Greeting from './components/Greeting';
 
-function App() {
-
-  const user1 = {
+const usersData = [
+  {
+    id: 0,
     firstName: 'User',
     lastName: 'Userenko',
     age: 23,
-  };
+  },
 
-  const user2 = {
+  {
+    id: 1,
     firstName: 'Anton',
     lastName: 'Antonov',
     age: 12,
-  };
+  },
 
-  const user3 = {
+  {
+    id: 2,
     firstName: 'John',
     lastName: 'Doe',
     age: 9887,
+  },
+];
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: usersData,
+      isDirectSort: true,
+    };
+
+    // this.sortUsers = this.sortUsers.bind(this);
+  }
+
+  sortUsers = () => {
+    const { isDirectSort, users } = this.state;
+
+    /*
+      1. взять массив
+      2. перевернуть его
+      3. обновить состояние
+    */
+
+    // const copy = [...users];
+
+    // const deepCopy = JSON.parse(JSON.stringify(users));
+    const usersCopy = structuredClone(users);
+
+    // usersCopy.reverse();
+
+    usersCopy.sort((a, b) => {
+      if (isDirectSort) {
+        return b.id - a.id;
+      }
+
+      return a.id - b.id;
+    });
+
+    this.setState({
+      users: usersCopy,
+      isDirectSort: !isDirectSort,
+    });
   };
-  
-  return (
-    <>
-      <Heading headerText='header text prop' headerTitle='header title prop' />
-      <Greeting user={user1} />
-      <Greeting user={user2} />
-      <Greeting user={user3} />
-    </>
-  );
+
+  render() {
+    const { users, isDirectSort } = this.state;
+
+    const greetingsArr = users.map((user) => (
+      <Greeting key={user.id} user={user} />
+    ));
+
+    return (
+      <>
+        <Heading
+          headerText='header text prop'
+          headerTitle='header title prop'
+        />
+        <div>
+          <p>Current sort order is {isDirectSort ? 'direct' : 'reversed'}</p>
+          <button onClick={this.sortUsers}>Toggle sorting order</button>
+        </div>
+        {greetingsArr}
+      </>
+    );
+  }
 }
 
 /*
