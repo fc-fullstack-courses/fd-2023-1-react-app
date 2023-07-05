acb52ce850d848b291f82974910aa5441936827d
Kyrylo <fresh.js.ftd.@gmail.com>
2023-07-05 16:32:00 +0300

    Add array query string example

--- src/api/index.js
+++ src/api/index.js
@@ -6,7 +6,8 @@ export const getUsers = (options) => {
     page: 1,
     results: 20,
     format: 'json',
-    seed: 'fd-2023-1'
+    seed: 'fd-2023-1',
+    inc: ['name', 'email','login', 'gender', 'nat']
   }
 
   const finalOptions = {
@@ -14,7 +15,7 @@ export const getUsers = (options) => {
     ...options
   }
 
-  const query = queryString.stringify(finalOptions);
+  const query = queryString.stringify(finalOptions, {arrayFormat: 'comma'});
 
   return fetch(`https://randomuser.me/api/?${query}`).then((response) =>
     response.json()
