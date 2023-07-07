3bf8992b6ae305863c6ce986dc769b45806c75e4
Kyrylo <fresh.js.ftd.@gmail.com>
2023-07-07 15:12:27 +0300

    Add components theme change examples

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,20 +1,47 @@
 import React from 'react';
-import { UserContext } from '../../contexts';
+import classNames from 'classnames';
+import { UserContext, ThemeContext } from '../../contexts';
+import styles from './Sidebar.module.scss';
+import CONSTANTS from '../../constants';
+const { THEMES } = CONSTANTS;
 
 const Sidebar = (props) => {
   return (
     <UserContext.Consumer>
       {(user) => (
-        <div
-          style={{
-            padding: '20px',
-            border: '2px solid black',
-            maxWidth: '500px',
+        <ThemeContext.Consumer>
+          {([theme, switchTheme]) => {
+            const className = classNames({
+              [styles.lightTheme]: theme === THEMES.LIGHT_THEME,
+              [styles.darkTheme]: theme === THEMES.DARK_THEME,
+            });
+
+            const changeTheme = (e) => {
+              const {target: {value}} = e;
+
+              switchTheme(value);
+            }
+
+            return (
+              <div
+                className={className}
+                style={{
+                  padding: '20px',
+                  border: '2px solid black',
+                  maxWidth: '500px',
+                }}
+              >
+                <h2>Sidebar</h2>
+                <p>Your email is: {user.email}</p>
+
+                <select value={theme} onChange={changeTheme}>
+                  <option value={THEMES.LIGHT_THEME}>Light Theme</option>
+                  <option value={THEMES.DARK_THEME}>Dark Theme</option>
+                </select>
+              </div>
+            );
           }}
-        >
-          <h2>Sidebar</h2>
-          <p>Your email is: {user.email}</p>
-        </div>
+        </ThemeContext.Consumer>
       )}
     </UserContext.Consumer>
   );
--- src/components/Tree/Parent/Child/UserData/index.jsx
+++ src/components/Tree/Parent/Child/UserData/index.jsx
@@ -1,15 +1,37 @@
-import { UserContext } from '../../../../../contexts';
+import classNames from 'classnames';
+import { UserContext, ThemeContext } from '../../../../../contexts';
+import styles from './UserData.module.scss';
+import CONSTANTS from '../../../../../constants';
+
+const { THEMES } = CONSTANTS;
 
 function UserData(props) {
-  const render = (user) => (
-    <div style={{ padding: '20px', border: '2px solid red' }}>
-      <h2>UserData</h2>
-      <h3>
-        {user.firstName} {user.lastName}
-      </h3>
-    </div>
+  return (
+    <UserContext.Consumer>
+      {(user) => (
+        <ThemeContext.Consumer>
+          {([theme]) => {
+            const className = classNames({
+              [styles.lightTheme]: theme === THEMES.LIGHT_THEME,
+              [styles.darkTheme]: theme === THEMES.DARK_THEME,
+            });
+
+            return (
+              <div
+                className={className}
+                style={{ padding: '20px', border: '2px solid red' }}
+              >
+                <h2>UserData</h2>
+                <h3>
+                  {user.firstName} {user.lastName}
+                </h3>
+              </div>
+            );
+          }}
+        </ThemeContext.Consumer>
+      )}
+    </UserContext.Consumer>
   );
-  return <UserContext.Consumer>{render}</UserContext.Consumer>;
 }
 
 export default UserData;
