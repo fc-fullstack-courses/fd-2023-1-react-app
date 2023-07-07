f9c2d6876db88544326a1c9fdce6b6f8af2e8482
Kyrylo <fresh.js.ftd.@gmail.com>
2023-07-07 15:11:34 +0300

    Add enum example

--- src/constants.js
+++ src/constants.js
@@ -4,4 +4,11 @@ export default {
   API_KEY: 'fd-2023-1',
   DEFAULT_USER_DATA: ['name', 'email', 'login', 'gender', 'nat'],
   BASE_URL: 'https://randomuser.me/api/',
+  THEMES : {
+    LIGHT_THEME: "light",
+    DARK_THEME: "dark",
+    GREEN_THEME: "green"
+  }
 };
+// enum
+
