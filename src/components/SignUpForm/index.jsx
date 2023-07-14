030d1887aef7ee1b5b9e5267b38a07f67d5f6066
Kyrylo <fresh.js.ftd.@gmail.com>
2023-07-14 15:32:41 +0300

    Add useReducer example

--- src/components/SignUpForm/index.jsx
+++ src/components/SignUpForm/index.jsx
@@ -1,14 +1,74 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
+
+function reducer(state, action) {
+  switch (action.name) {
+    case 'login': {
+      const newState = {
+        ...state,
+        login: action.value,
+      };
+
+      return newState;
+    }
+    case 'password': {
+      const newState = {
+        ...state,
+        password: action.value,
+      };
+
+      return newState;
+    }
+    case 'email': {
+      const newState = {
+        ...state,
+        email: action.value,
+      };
+
+      return newState;
+    }
+    case 'phone': {
+      const newState = {
+        ...state,
+        phone: action.value,
+      };
+
+      return newState;
+    }
+    case 'name': {
+      const newState = {
+        ...state,
+        name: action.value,
+      };
+
+      return newState;
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
+const initialState = {
+  login: '',
+  password: '',
+  email: '',
+  phone: '',
+  name: '',
+};
 
 const SignUpForm = (props) => {
-  const [login, setLogin] = useState('');
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [name, setName] = useState('');
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  function handleChange (e) {
-    const { target: {value, name}} = e;
+  // console.log(state);
+
+  function handleChange(e) {
+    const {
+      target: { value, name },
+    } = e;
+
+    const action = { name, value };
+
+    dispatch(action);
   }
 
   return (
@@ -20,11 +80,41 @@ const SignUpForm = (props) => {
         padding: '20px',
       }}
     >
-      <input type='text' name='login' placeholder='login' value={login} onChange={handleChange} />
-      <input type='password' name='password' placeholder='password' />
-      <input type='email' name='email' placeholder='email' />
-      <input type='text' name='phone' placeholder='phone number' />
-      <input type='text' name='name' placeholder='name' />
+      <input
+        type='text'
+        name='login'
+        placeholder='login'
+        value={state.login}
+        onChange={handleChange}
+      />
+      <input
+        type='password'
+        name='password'
+        placeholder='password'
+        value={state.password}
+        onChange={handleChange}
+      />
+      <input
+        type='email'
+        name='email'
+        placeholder='email'
+        value={state.email}
+        onChange={handleChange}
+      />
+      <input
+        type='text'
+        name='phone'
+        placeholder='phone number'
+        value={state.phone}
+        onChange={handleChange}
+      />
+      <input
+        type='text'
+        name='name'
+        placeholder='name'
+        value={state.name}
+        onChange={handleChange}
+      />
       <button>Submit</button>
       <button>Reset</button>
     </form>
