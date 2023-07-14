dcd8948d17bdaf41f9036b7f8847a33b9f8893be
Kyrylo <fresh.js.ftd.@gmail.com>
2023-07-14 14:20:14 +0300

    Add useCallback example

--- src/components/Tree/Parent/Child/UserData/index.jsx
+++ src/components/Tree/Parent/Child/UserData/index.jsx
@@ -1,7 +1,13 @@
 import classNames from 'classnames';
 import styles from './UserData.module.scss';
 import CONSTANTS from '../../../../../constants';
-import React, { useContext, useMemo, useState } from 'react';
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
 import { ThemeContext, UserContext } from '../../../../../contexts';
 
 const { THEMES } = CONSTANTS;
@@ -9,7 +15,7 @@ const { THEMES } = CONSTANTS;
 function longCalculations(value) {
   let i = 0;
 
-  for(let j = 0; j < 3_000_000_000; j++) {
+  for (let j = 0; j < 3_000_000_000; j++) {
     i++;
   }
 
@@ -21,13 +27,25 @@ function UserData(props) {
   const [theme] = useContext(ThemeContext);
   const user = useContext(UserContext);
 
+  const showValue = useCallback(function showValue() {
+    alert(`value is ${value}`);
+  }, [value]);
+
+  useEffect(() => {
+    console.log('showValue created');
+  }, [showValue]);
+
+  // function showValue() {
+  //   alert(`value is ${value}`);
+  // }
+
+  const result = useMemo(() => longCalculations(value), [value]);
+  // const result = longCalculations(value);
+
   const className = classNames({
     [styles.lightTheme]: theme === THEMES.LIGHT_THEME,
     [styles.darkTheme]: theme === THEMES.DARK_THEME,
   });
-
-  const result = useMemo(() => longCalculations(value), [value]);
-  // const result = longCalculations(value);
 
   return (
     <div
@@ -44,6 +62,7 @@ function UserData(props) {
           onChange={({ target: { value } }) => setValue(+value)}
         />
         <p>{result}</p>
+        <button onClick={showValue}>Show value</button>
       </div>
     </div>
   );
