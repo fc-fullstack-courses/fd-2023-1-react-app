999bbdd84806bdf11bee8257e4e9b872f1ced87b
Kyrylo <fresh.js.ftd.@gmail.com>
2023-07-18 16:40:07 +0300

    Add formik error handling

--- src/components/SignUpForm/index.jsx
+++ src/components/SignUpForm/index.jsx
@@ -18,27 +18,46 @@ const SignUpForm = (props) => {
   };
 
   return (
-    <Formik initialValues={initialValues} onSubmit={handleSignUp}>
-      <Form
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          maxWidth: '400px',
-          padding: '20px',
-        }}
-      >
-        <Field type='text' name='firstName' placeholder='firstName' />
-        <Field type='text' name='lastName' placeholder='lastName' />
-        <Field type='email' name='email' placeholder='email' />
-        <Field type='password' name='password' placeholder='password' />
-        <Field as='select' name='accountLevel'>
-          <option value="basic">Basic</option>
-          <option value="advanced">Advanced</option>
-          <option value="Pro">Pro</option>
-        </Field>
-        <button type='submit'>Submit</button>
-        <button type='reset'>Reset</button>
-      </Form>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSignUp}
+      validationSchema={USER_SCHEMA}
+    >
+      {(props) => {
+        console.log(props);
+        return (
+          <Form
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              maxWidth: '400px',
+              padding: '20px',
+            }}
+          >
+            <Field type='text' name='firstName' placeholder='firstName' />
+            <ErrorMessage name='firstName'>
+              {(msg) => <div style={{ color: 'red' }}>{msg}</div>}
+            </ErrorMessage>
+            <Field type='text' name='lastName' placeholder='lastName' />
+            <ErrorMessage
+              name='lastName'
+              component='div'
+              style={{ color: 'red' }}
+            />
+            <Field type='email' name='email' placeholder='email' />
+            <ErrorMessage name='email' render={(msg) => <div>{msg}</div>} />
+            <Field type='password' name='password' placeholder='password' />
+            <ErrorMessage name='password' />
+            <Field as='select' name='accountLevel'>
+              <option value='basic'>Basic</option>
+              <option value='advanced'>Advanced</option>
+              <option value='Pro'>Pro</option>
+            </Field>
+            <button type='submit'>Submit</button>
+            <button type='reset'>Reset</button>
+          </Form>
+        );
+      }}
     </Formik>
   );
 };
