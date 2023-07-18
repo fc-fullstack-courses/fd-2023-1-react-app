4eac0146cdeed8b489c333d79d78f6873ad9c137
Kyrylo <fresh.js.ftd.@gmail.com>
2023-07-18 16:09:55 +0300

    Add basic formik form example

--- src/components/SignUpForm/index.jsx
+++ src/components/SignUpForm/index.jsx
@@ -1,93 +1,39 @@
 import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { USER_SCHEMA } from 'utils/validation/userValidation';
 
-
-const user1 = {
-  firstName: 'User',
-  lastName: 'Userenko',
-  email: 'mail@mail.com',
-  password: 'User1IsBest$$##'
-};
-
-const user2 = {
-  firstName: 545,
-  email: 'user2',
-  password: '12345admin'
+const initialValues = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  password: '',
 };
 
 const SignUpForm = (props) => {
-
-  // console.group();
-  // // Возвращают булевое значение, подходит ли обьект под схему
-  // console.log('User 1 isValid');
-  // console.log(USER_SCHEMA.isValid(user1));
-  // console.log('User 1 isValidSync');
-  // console.log(USER_SCHEMA.isValidSync(user1));
-  // console.log('User 2 isValid');
-  // console.log(USER_SCHEMA.isValid(user2));
-  // console.log('User 2 isValidSync');
-  // console.log(USER_SCHEMA.isValidSync(user2));
-  // console.groupEnd();
-
-  console.group();
-  // Возвращают либо прошедший проверку обьект или ошибку
-  console.log('User 1 validate');
-  console.log(USER_SCHEMA.validate(user1));
-  console.log('User 1 validateSync');
-  console.log(USER_SCHEMA.validateSync(user1));
-  console.log('User 2 validate');
-  console.log(USER_SCHEMA.validate(user2));
-  console.log('User 2 validateSync');
-  console.log(USER_SCHEMA.validateSync(user2));
-  console.groupEnd();
+  const handleSignUp = (values, formikBag) => {
+    console.log(values);
+    console.log(formikBag);
+    formikBag.resetForm();
+  };
 
   return (
-    <form
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        maxWidth: '400px',
-        padding: '20px',
-      }}
-    >
-      {/* <input
-        type='text'
-        name='login'
-        placeholder='login'
-        value={state.login}
-        onChange={handleChange}
-      />
-      <input
-        type='password'
-        name='password'
-        placeholder='password'
-        value={state.password}
-        onChange={handleChange}
-      />
-      <input
-        type='email'
-        name='email'
-        placeholder='email'
-        value={state.email}
-        onChange={handleChange}
-      />
-      <input
-        type='text'
-        name='phone'
-        placeholder='phone number'
-        value={state.phone}
-        onChange={handleChange}
-      />
-      <input
-        type='text'
-        name='name'
-        placeholder='name'
-        value={state.name}
-        onChange={handleChange}
-      /> */}
-      <button>Submit</button>
-      <button>Reset</button>
-    </form>
+    <Formik initialValues={initialValues} onSubmit={handleSignUp}>
+      <Form
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          maxWidth: '400px',
+          padding: '20px',
+        }}
+      >
+        <Field type='text' name='firstName' placeholder='firstName' />
+        <Field type='text' name='lastName' placeholder='lastName' />
+        <Field type='email' name='email' placeholder='email' />
+        <Field type='password' name='password' placeholder='password' />
+        <button type='submit'>Submit</button>
+        <button type='reset'>Reset</button>
+      </Form>
+    </Formik>
   );
 };
 
